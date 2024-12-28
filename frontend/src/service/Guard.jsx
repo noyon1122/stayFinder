cb4cef84e72191a4adda804a39dfc4a6f4eb034f
noyon122 <noyonhossain910@gmail.com>
2024-12-28 20:46:14 +0600

    there some work with frontend

---
+++ frontend/src/service/Guard.jsx
@@ -0,0 +1,28 @@
+
+// src/ProtectedRoute.js
+import React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import ApiService from './ApiService';
+
+
+export const ProtectedRoute = ({ element: Component }) => {
+  const location = useLocation();
+
+  return ApiService.isAuthenticated() ? (
+    Component
+  ) : (
+
+   <Navigate to={"/login"} replace state={{form: location}}></Navigate>
+  );
+};
+
+
+export const AdminRoute = ({ element: Component }) => {
+  const location = useLocation();
+
+  return ApiService.isAdmin() ? (
+    Component
+  ) : (
+    <Navigate to="/login" replace state={{ from: location }} />
+  );
+};
